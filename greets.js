58f0f3b8ba3282a1144b4864f7236c1c925d8fde
Felipe Tenório <felipetenorio@gmail.com>
2022-04-06 00:21:08 -0300

    Update greets.js
    greet

--- greets.js
+++ greets.js
@@ -4,94 +4,94 @@ module.exports = [
 
             {
                 id: 1,
-                excuse:"BOM DIA MEUS ESCULTORES DE MORINGAS",
+                greet:"BOM DIA MEUS ESCULTORES DE MORINGAS",
                 category:"original",
                 
             },
             {
                 id:2,
-                excuse:"BOM DIA MEUS ESTIMADOS REMOVEDORES DE PESPEGOS",
+                greet:"BOM DIA MEUS ESTIMADOS REMOVEDORES DE PESPEGOS",
                 category:"original",
             },
             {
                 id:3,
-                excuse:"BOM DIA MEUS VIAJANTES DO OUMUAMUA",
+                greet:"BOM DIA MEUS VIAJANTES DO OUMUAMUA",
                 category:"original",
             },
             {
                 id:4,
-                excuse:"BOM DIA MEUS VERSADOS NO MANUSCRITO VOYNICH",
+                greet:"BOM DIA MEUS VERSADOS NO MANUSCRITO VOYNICH",
                 category:"original",
             },
             {
                 id:5,
-                excuse:"BOM DIA MEUS INCRÍVEIS ESTUDIOSOS DA ESCALA KARDASHEV",
+                greet:"BOM DIA MEUS INCRÍVEIS ESTUDIOSOS DA ESCALA KARDASHEV",
                 category:"original",
             },
             {
                 id:6,
-                excuse:"BOM DIA MEUS INSTRUÍDOS DESASNADOS",
+                greet:"BOM DIA MEUS INSTRUÍDOS DESASNADOS",
                 category:"original",
             },
             {
                 id:7,
-                excuse:"BOM DIA MEUS NEGOCIANTE DE BUFARINHAS EXÓTICAS",
+                greet:"BOM DIA MEUS NEGOCIANTE DE BUFARINHAS EXÓTICAS",
                 category:"original",
             },
             {
                 id:8,
-                excuse:"BOM DIA MEUS ADSTRITOS A BOAS ENERGIAS",
+                greet:"BOM DIA MEUS ADSTRITOS A BOAS ENERGIAS",
                 category:"original",
             },
             {
                 id:9,
-                excuse:"BOM DIA MEUS CRÍTICOS DE MANGUARIS",
+                greet:"BOM DIA MEUS CRÍTICOS DE MANGUARIS",
                 category:"original",
             },
             {
                 id:10,
-                excuse:"BOM DIA MEUS DISTRIBUIDORES DE BENEPLÁCITOS",
+                greet:"BOM DIA MEUS DISTRIBUIDORES DE BENEPLÁCITOS",
                 category:"original",
             },
             {
                 id:11,
-                excuse:"BOM DIA MEUS ESTORCEGADORES DE CONSCIÊNCIAS",
+                greet:"BOM DIA MEUS ESTORCEGADORES DE CONSCIÊNCIAS",
                 category:"original",
             },
 
             {
                 id:12,
-                excuse:"BOM DIA MEUS AMIGOS DE FEITOS ESTONTEANTES",
+                greet:"BOM DIA MEUS AMIGOS DE FEITOS ESTONTEANTES",
                 category:"original",
             },
 
             {
                 id:13,
-                excuse:"BOM DIA MEUS TRATADORES DE ISCNOFONIA",
+                greet:"BOM DIA MEUS TRATADORES DE ISCNOFONIA",
                 category:"original",
             },
 
             {
                 id:14,
-                excuse:"BOM DIA MEUS ACOBILHADORES DE ENERGIAS POSITIVAS",
+                greet:"BOM DIA MEUS ACOBILHADORES DE ENERGIAS POSITIVAS",
                 category:"original",
             },
 
             {
                 id:15,
-                excuse:"BOM DIA MEUS CATALOGADORES DE SERES ABLÉFAROS",
+                greet:"BOM DIA MEUS CATALOGADORES DE SERES ABLÉFAROS",
                 category:"original",
             },
 
             {
                 id:16,
-                excuse:"BOM DIA QUERIDAS PESSOAS DOTADAS DE OSFRESIA",
+                greet:"BOM DIA QUERIDAS PESSOAS DOTADAS DE OSFRESIA",
                 category:"original",
             },
 // Custom
             {
                 id:17,
-                excuse:"BOM DIA MEUS SERES DOTADOS DE APANÁGIOS",
+                greet:"BOM DIA MEUS SERES DOTADOS DE APANÁGIOS",
                 category:"custom",
             },
 
